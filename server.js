6bd10e424e5ac616fe564efc9a462303d3ed0174
Ubilov <ubilov@gmail.com>
2025-06-30 23:02:40 -0300

    Fix static file serving and correct image path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname)));
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
